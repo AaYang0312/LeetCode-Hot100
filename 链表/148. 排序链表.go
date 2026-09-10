0e23922eb7c1666ec2eb162e15a12909933d348b
AaYang0312 <liu20031221@outlook.com>
2026-09-10 18:59:44 +0800

    9.10 修改146、148链表解法

--- 链表/148. 排序链表.go
+++ 链表/148. 排序链表.go
@@ -1,70 +1,52 @@
 package main
 
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
 func sortList(head *ListNode) *ListNode {
-	if head == nil {
+	if head == nil || head.Next == nil {
 		return head
 	}
-	length := 0
-	for node := head; node != nil; node = node.Next {
-		length++
-	}
-	dummyHead := &ListNode{Next: head}
-
-	// 子串长度
-	for subLength := 1; subLength < length; subLength <<= 1 {
-		// 每次sublength变化都要初始化
-		prev, curr := dummyHead, dummyHead.Next
-		for curr != nil {
-			head1 := curr
-			// 找到第一个子串尾部
-			for i := 1; i < subLength && curr.Next != nil; i++ {
-				curr = curr.Next
-			}
-			head2 := curr.Next
-			// 切断串间连接
-			curr.Next = nil
-			curr = head2
-			// 找到第2个子串尾部
-			for i := 1; i < subLength && curr != nil && curr.Next != nil; i++ {
-				curr = curr.Next
-			}
-			var nextHead *ListNode
-			if curr != nil {
-				nextHead = curr.Next
-				curr.Next = nil
-			}
-			prev.Next = Merge(head1, head2)
-			// prev 放到队尾
-			for prev.Next != nil {
-				prev = prev.Next
-			}
-			curr = nextHead // nexthead存在则为下一子串开头，否则为nil
-
-		}
-
-	}
-	return dummyHead.Next
-
+	// 找中间节点，断开前一个
+	head2 := middleNode(head)
+	// 分治
+	head1 := sortList(head)
+	head2 = sortList(head2)
+	// 合并
+	return mergeLists(head1, head2)
 }
-func Merge(head1, head2 *ListNode) *ListNode {
-	dummy := &ListNode{}
-	temp1, temp2 := head1, head2
-	curr := dummy
-	for temp1 != nil && temp2 != nil {
-		if temp1.Val <= temp2.Val {
-			curr.Next = temp1
-			temp1 = temp1.Next
-			curr = curr.Next
-		} else {
-			curr.Next = temp2
-			temp2 = temp2.Next
-			curr = curr.Next
-		}
+func middleNode(head *ListNode) *ListNode {
+	slow, pre, fast := head, head, head
+	for fast != nil && fast.Next != nil {
+		pre = slow
+		slow = slow.Next
+		fast = fast.Next.Next
 	}
-	if temp1 != nil {
-		curr.Next = temp1
-	} else if temp2 != nil {
-		curr.Next = temp2
+	pre.Next = nil // 断开
+	return slow
+}
+func mergeLists(head1, head2 *ListNode) *ListNode {
+	dummy := &ListNode{}
+	curr := dummy
+	for head1 != nil && head2 != nil {
+		if head1.Val <= head2.Val {
+			curr.Next = head1
+			head1 = head1.Next
+		} else {
+			curr.Next = head2
+			head2 = head2.Next
+		}
+		curr = curr.Next
+	}
+	if head1 != nil {
+		curr.Next = head1
+	}
+	if head2 != nil {
+		curr.Next = head2
 	}
 	return dummy.Next
 }
